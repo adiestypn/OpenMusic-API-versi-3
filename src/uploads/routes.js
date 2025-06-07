0a1ca2db5a03967c70201de2d6db1b476e9a5667
Adiesty <adiestypuspa@gmail.com>
2025-06-07 18:54:46 +0700

    fix: lokasi file terbaca

--- src/uploads/routes.js
+++ src/uploads/routes.js
@@ -3,25 +3,26 @@ const path = require('path');
 const routes = (handler) => [
   {
     method: 'POST',
-    path: '/upload/images',
+    path: '/albums/{id}/covers',
     handler: handler.postUploadImageHandler,
     options: {
       payload: {
         allow: 'multipart/form-data',
         multipart: true,
         output: 'stream',
+        maxBytes: 512000, 
       },
     },
   },
   {
     method: 'GET',
-    path: '/upload/{param*}',
+    path: '/uploads/images/{param*}',
     handler: {
       directory: {
-        path: path.resolve(__dirname, 'file'),
+        path: path.resolve(__dirname, 'file/images'),
       },
     },
-  },
+  }
 ];
  
 module.exports = routes;
